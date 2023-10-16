94197e40ea57762c7643d5183744dd77a503e937
SurinderArora7 <arorasurinder521@gmail.com>
2023-10-16 14:45:39 +0530

    success

---
+++ ORSProject-10/src/main/java/com/rays/common/attachment/AttachmentSummaryDTO.java
@@ -0,0 +1,15 @@
+package com.rays.common.attachment;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Contains attached file information and data
+ * @author Surinder Arora
+ */
+
+@Entity
+@Table(name = "NCS_ATTACHMENT")
+public class AttachmentSummaryDTO extends AttachmentBaseDTO {
+
+}
